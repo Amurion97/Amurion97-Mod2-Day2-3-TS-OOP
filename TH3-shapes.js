8ce09a1cf50d0c58b61bca980f1da018229d2022
Van-Giang NGUYEN <van.giang.nguyen@outlook.com.vn>
2023-02-10 13:41:09 +0700

    TH3

---
+++ TH3-shapes.js
@@ -0,0 +1,70 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Shape = /** @class */ (function () {
+    function Shape(color, filled) {
+        this._color = color;
+        this._filled = filled;
+    }
+    Object.defineProperty(Shape.prototype, "color", {
+        get: function () {
+            return this._color;
+        },
+        set: function (value) {
+            this._color = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Shape.prototype, "filled", {
+        get: function () {
+            return this._filled;
+        },
+        set: function (value) {
+            this._filled = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Shape;
+}());
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(color, filled, radius) {
+        var _this = _super.call(this, color, filled) || this;
+        _this._radius = radius;
+        return _this;
+    }
+    Object.defineProperty(Circle.prototype, "radius", {
+        get: function () {
+            return this._radius;
+        },
+        set: function (value) {
+            this._radius = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Circle.prototype.getArea = function () {
+        return (Math.pow(this.radius, 2)) * Math.PI;
+    };
+    Circle.prototype.getPerimeter = function () {
+        return (this.radius * 2) * Math.PI;
+    };
+    return Circle;
+}(Shape));
+var circleA = new Circle("yellow", true, 5);
+console.log(circleA.getArea());
+console.log(circleA.getPerimeter());
---
+++ TH3-shapes.ts
@@ -0,0 +1,53 @@
+class Shape {
+    private _color: string;
+    private _filled: boolean;
+
+    constructor(color: string, filled: boolean) {
+        this._color = color;
+        this._filled = filled;
+    }
+
+    get color(): string {
+        return this._color;
+    }
+
+    set color(value: string) {
+        this._color = value;
+    }
+
+    get filled(): boolean {
+        return this._filled;
+    }
+
+    set filled(value: boolean) {
+        this._filled = value;
+    }
+}
+class Circle extends Shape {
+    private _radius: number;
+
+    constructor(color: string, filled: boolean, radius: number) {
+        super(color, filled);
+        this._radius = radius;
+    }
+
+    get radius(): number {
+        return this._radius;
+    }
+
+    set radius(value: number) {
+        this._radius = value;
+    }
+
+    getArea ():number {
+        return (this.radius**2)*Math.PI;
+    }
+
+    getPerimeter(): number {
+        return (this.radius*2)*Math.PI;
+    }
+}
+
+let circleA: Circle = new Circle("yellow", true, 5);
+console.log(circleA.getArea());
+console.log(circleA.getPerimeter());
